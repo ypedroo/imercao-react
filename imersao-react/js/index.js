c149582b93a9197fd8cd51af2720e44587741ec4
ypedro <ynoa.pedro@outlook.com>
2020-07-20 20:05:08 -0300

    Initial commit

---
+++ imersao-react/js/index.js
@@ -0,0 +1,10 @@
+//BOM - window
+//DOM - document 
+const root = document.querySelector('#root');
+const title = '<h1>Difficulty</h1>'
+
+root.insertAdjacentHTML('beforeend', title);
+root.insertAdjacentHTML('beforeend', title);
+root.insertAdjacentHTML('beforeend', title);
+root.insertAdjacentHTML('beforeend', title);
+root.insertAdjacentHTML('beforeend', title);
