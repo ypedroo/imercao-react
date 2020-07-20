7e8e1fc74f8421a00a0a7608a6f0f2027d012165
ypedro <ynoa.pedro@outlook.com>
2020-07-20 20:19:58 -0300

    small changes

--- imersao-react/js/index.js
+++ imersao-react/js/index.js
@@ -1,10 +1,14 @@
 //BOM - window
 //DOM - document 
 const root = document.querySelector('#root');
-const title = '<h1>Difficulty</h1>'
 
-root.insertAdjacentHTML('beforeend', title);
-root.insertAdjacentHTML('beforeend', title);
-root.insertAdjacentHTML('beforeend', title);
-root.insertAdjacentHTML('beforeend', title);
-root.insertAdjacentHTML('beforeend', title);
+function newElement(tag, content) {
+    const element = `<${tag}>${content}</${tag}>`
+    
+    root.insertAdjacentHTML('beforeend', element);
+    
+}
+
+
+newElement('h1', 'Difficulty')
+newElement('h2', 'ypedro')
